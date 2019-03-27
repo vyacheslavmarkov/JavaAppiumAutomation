c2e53c3d0c1fed81244c2bb78a3eb89c247cc613
vmarkov <vmarkov@weezlabs.com>
2019-03-27 17:39:55 +0300

    Lesson 6, 1

--- src/lib/tests/GetStartedTest.java
+++ src/lib/tests/GetStartedTest.java
@@ -1,13 +1,16 @@
-package lib.tests.iOS;
+package lib.tests;
 
-import lib.iOSTestCase;
+import lib.CoreTestCase;
 import lib.ui.WelcomePageObject;
 import org.junit.Test;
 
-public class GetStartedTest extends iOSTestCase {
+public class GetStartedTest extends CoreTestCase {
 
     @Test
     public void testPassThroughWelocme() {
+        if (this.Platform.isAndroid()) {
+            return;
+        }
         WelcomePageObject WelcomePage = new WelcomePageObject(driver);
 
         WelcomePage.waitForLearnMoreLink();
